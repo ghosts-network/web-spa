20fbe2ab807d3d7a4df6eee02d9f22126c4940fb
vladimirborodin <boberneprotiv@gmail.com>
2020-11-18 19:07:17 +0200

    Configure prod environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-import {ApiModule} from './modules/gateway-api';
+import {ApiModule, Configuration} from './modules/gateway-api';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {environment} from "../environments/environment";
 
 @NgModule({
   declarations: [
@@ -17,7 +18,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
-    ApiModule,
+    ApiModule.forRoot(() => new Configuration({
+      basePath: environment.basePath
+    })),
     BrowserAnimationsModule
   ],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  basePath: 'http://boberneprotiv.com:5000'
 };
