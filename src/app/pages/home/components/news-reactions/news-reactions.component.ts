3546885a1dc80b7146f5652cd549e937b1433005
Alexander <50077233+Sekir0@users.noreply.github.com>
2020-11-23 15:48:24 +0200

    add display of reactions

--- src/app/pages/home/components/news-reactions/news-reactions.component.ts
+++ src/app/pages/home/components/news-reactions/news-reactions.component.ts
@@ -2,11 +2,13 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import {AddNewsFeedReaction, NewsFeedPublication, NewsFeedService, PublicationComment, ReactionType} from '../../../../modules/gateway-api';
 
 @Component({
-  selector: 'app-reactions',
-  templateUrl: './reactions.component.html',
-  styleUrls: ['./reactions.component.scss']
+  selector: 'app-news-reactions',
+  templateUrl: './news-reactions.component.html',
+  styleUrls: ['./news-reactions.component.scss']
 })
 export class ReactionsComponent {
+
+  hide = true;
 
   constructor(private newsFeedService: NewsFeedService) { }
 
@@ -17,6 +19,16 @@ export class ReactionsComponent {
     this.newsFeedService.newsFeedPublicationIdReactionPost(this.publication.id, {reaction})
     .subscribe(resp => {
       this.publication.reactions.reactions.push(resp);
+      this.publication.reactions.totalCount += 1;
     });
+  }
+
+  public hidden(): void {
+    if (this.hide === true){
+      setTimeout(() => {this.hide = false; }, 500);
+    }
+    else {
+      setTimeout(() => {this.hide = true; }, 1000);
+    }
   }
 }
