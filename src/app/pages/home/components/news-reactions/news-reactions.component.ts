97f0bce0cc847fce0055d3dbe2e4c7041ac42b47
Alexander <50077233+Sekir0@users.noreply.github.com>
2020-12-22 21:06:59 +0200

    upd reactions
    user reaction is now displayed on the button
    +delete reaction method
    upd reactions style

--- src/app/pages/home/components/news-reactions/news-reactions.component.ts
+++ src/app/pages/home/components/news-reactions/news-reactions.component.ts
@@ -1,6 +1,6 @@
+import { ReactionButtonEnum, ReactionsEnum } from './../../entities/reactions.enum';
 import { Component, Input } from '@angular/core';
 import { NewsFeedPublication, NewsFeedService, ReactionType} from '../../../../modules/gateway-api';
-import {ReactionsEnum} from "../../entities/reactions.enum";
 
 @Component({
   selector: 'app-news-reactions',
@@ -19,13 +19,30 @@ export class ReactionsComponent {
   public addReaction(reaction: ReactionType): void {
     this.newsFeedService.newsFeedPublicationIdReactionPost(this.publication.id, {reaction})
     .subscribe(resp => {
-      this.publication.reactions.reactions = resp[0].reactions;
-      this.publication.reactions.totalCount = resp[0].totalCount;
+      this.publication.reactions.reactions = resp.reactions;
+      this.publication.reactions.totalCount = resp.totalCount;
+      this.publication.reactions.user = resp.user;
+    });
+  }
+
+  public deleteReaction(): void {
+    this.newsFeedService.newsFeedPublicationIdReactionDelete(this.publication.id)
+    .subscribe(resp => {
+      this.publication.reactions.reactions = resp.reactions;
+      this.publication.reactions.totalCount = resp.totalCount;
+      this.publication.reactions.user = resp.user;
     });
   }
 
   public get availableReactions() {
-    return [0, 1, 2, 3, 4, 5].map(v => { return {index: v, url: ReactionsEnum[v]} });
+    return [0, 1, 2, 3, 4, 5].map(v => { return {index: v, url: ReactionButtonEnum[v]} });
+  }
+
+  public get userReaction() {
+    if (this.publication.reactions.user == null) {
+      return;
+    }
+    return ReactionButtonEnum[this.publication.reactions.user.type]
   }
 
   public hidden(): void {
