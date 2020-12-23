97a70cd79a720938fa6a63b80ff40fcd9b37c236
Vladimir <Boberneprotiv@gmail.com>
2020-12-23 18:55:26 +0200

    Update auth client name

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   basePath: 'http://boberneprotiv.com:5000',
   auth: {
     authority: 'http://boberneprotiv.com:6030',
-    client_id: 'angular_spa',
+    client_id: 'angular_spa_prod',
     redirect_uri: 'http://boberneprotiv.com/auth-callback',
     post_logout_redirect_uri: 'http://boberneprotiv.com/',
     response_type: "id_token token",
