784be3d8beede08972a1d440aad09193d3c8a7b8
Vladimir <Boberneprotiv@gmail.com>
2021-01-01 09:21:04 +0200

    FIx pro scopes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
     redirect_uri: 'https://gn.boberneprotiv.com/auth-callback',
     post_logout_redirect_uri: 'https://gn.boberneprotiv.com/',
     response_type: "id_token token",
-    scope: "openid profile",
+    scope: "openid profile api",
     filterProtocolClaims: true,
     loadUserInfo: true
   }
