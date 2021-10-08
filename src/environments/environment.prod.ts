311abf3464ad0cdc809fcc0d8fb8e8784e3079d0
Vladimir <Boberneprotiv@gmail.com>
2021-10-08 14:10:43 +0300

    Update base URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  basePath: 'https://api.gn.boberneprotiv.com',
+  basePath: 'https://api.ghost-network.boberneprotiv.com',
   auth: {
-    authority: 'https://account.gn.boberneprotiv.com',
+    authority: 'https://accounts.ghost-network.boberneprotiv.com',
     client_id: 'angular_spa_prod',
-    redirect_uri: 'https://gn.boberneprotiv.com/auth-callback',
-    post_logout_redirect_uri: 'https://gn.boberneprotiv.com/',
+    redirect_uri: 'https://ghost-network.boberneprotiv.com/auth-callback',
+    post_logout_redirect_uri: 'https://ghost-network.boberneprotiv.com/',
     response_type: "id_token token",
     scope: "openid profile api",
     filterProtocolClaims: true,
