204732b4bc438afbe6fbe315be1aa059d0f76a89
Bohdan Kolomeiets <38009812+bod9i@users.noreply.github.com>
2021-11-08 17:28:31 +0200

    create NewsCommentItemComponen

---
+++ src/app/modules/shared/components/news-list/components/news-comment-item/news-comment-item.component.ts
@@ -0,0 +1,51 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Profile } from 'oidc-client';
+import { PublicationComment } from '../../../../../gateway-api';
+import {TimeLimitCheker} from '../../classes/timeLimitCheker';
+
+@Component({
+  selector: 'app-news-comment-item',
+  templateUrl: './news-comment-item.component.html',
+  styleUrls: ['./news-comment-item.component.scss']
+})
+export class NewsCommentItemComponent implements OnInit {
+
+  public isEditNow: Boolean = false;
+  public form: FormGroup;
+  public timeLimitChecker: TimeLimitCheker = new TimeLimitCheker();
+
+  @Input()
+  public currentUser: Profile;
+  @Input()
+  public comment: PublicationComment;
+
+  @Output()
+  onDeleted = new EventEmitter<PublicationComment>();
+  @Output()
+  onEdited = new EventEmitter<PublicationComment>();
+
+  constructor(private fb: FormBuilder) {
+    this.form = fb.group({
+      content: ['', [Validators.required]]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  public deleteComment() {
+    this.onDeleted.emit(this.comment);
+  }
+
+  public editComment() {
+    this.isEditNow = true;
+    this.form.get('content').setValue(this.comment.content);
+  }
+
+  public editSubmitted() {
+    if (this.form.valid) {
+      this.comment.content = this.form.get('content').value;
+      this.onEdited.emit(this.comment);
+    }
+  }
+}
