7fbd51593a657ce658be26bf9f085eb35cae2f59
Bohdan Kolomeiets <38009812+bod9i@users.noreply.github.com>
2022-03-10 21:21:01 +0200

    implement actions with users

--- src/app/pages/profile/components/profiles-list/profiles-list.component.ts
+++ src/app/pages/profile/components/profiles-list/profiles-list.component.ts
@@ -1,7 +1,10 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Profile, User } from 'oidc-client';
 import { take } from 'rxjs/operators';
 import { RelationsService } from 'src/app/modules/gateway-api';
 import { UserInfo } from 'src/app/modules/gateway-api/model/userInfo';
+import { AuthService } from 'src/app/providers/services/auth/auth.service';
 
 @Component({
   selector: 'app-profiles-list',
@@ -11,14 +14,43 @@ import { UserInfo } from 'src/app/modules/gateway-api/model/userInfo';
 export class ProfilesListComponent implements OnInit {
   public DefaultAvatar = 'https://material.angular.io/assets/img/examples/shiba1.jpg';
   public panelOpenState: boolean;
+  public currentUserId: string;
   @Input()
   public label: string;
   @Input()
+  public approving: boolean = false;
+  @Input()
+  public removing: boolean = false;
+  @Input()
   public users: UserInfo[];
+
   @Output()
   public listChanged = new EventEmitter<UserInfo[]>();
+  @Output()
+  public approved = new EventEmitter<UserInfo>();
+  @Output()
+  public declined = new EventEmitter<UserInfo>();
+  @Output()
+  public removed = new EventEmitter<UserInfo>();
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.authService.getProfile()
+      .subscribe(user => {
+        this.currentUserId = user.sub;
+      });
+  }
+
+  public onApprove(user: UserInfo) : void {
+    this.approved.emit(user);
+  }
+
+  public onDecline(user: UserInfo) : void {
+    this.declined.emit(user);
+  }
+
+  public onRemove(user: UserInfo) : void {
+    this.removed.emit(user);
+  }
 }
