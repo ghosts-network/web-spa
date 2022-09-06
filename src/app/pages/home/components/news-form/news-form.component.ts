bf66b0d0ac290e3fc9c081910e827e0035c07b42
boberneprotiv <boberneprotiv@gmail.com>
2022-09-06 12:15:14 +0300

    inital inspector integration

--- src/app/pages/home/components/news-form/news-form.component.ts
+++ src/app/pages/home/components/news-form/news-form.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit, EventEmitter, Output} from '@angular/core';
 import {UntypedFormBuilder, UntypedFormGroup, Validators} from '@angular/forms';
 import {NewsFeedPublication, NewsFeedService} from '../../../../modules/gateway-api';
+import {interval} from 'rxjs';
+import {debounce} from 'rxjs/operators';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-news-form',
@@ -15,9 +18,28 @@ export class NewsFormComponent implements OnInit {
   public OnPublished = new EventEmitter<NewsFeedPublication>();
 
   constructor(private fb: UntypedFormBuilder,
-              private newsFeedService: NewsFeedService) {
+              private newsFeedService: NewsFeedService,
+              private httpClient: HttpClient) {
     this.form = fb.group({
       content: ['', [Validators.required]]
+    });
+
+    this.form.get('content').valueChanges
+      .pipe(debounce(() => interval(1000)))
+      .subscribe(s => {
+      console.log(s);
+      const regexp = /(https?):\/\/(www\.)?[a-z0-9\.:].*?(?=\s)/g;
+      console.log([...s.matchAll(regexp)]);
+
+      const body = new URLSearchParams();
+      body.set('url', s);
+      const options = {
+        headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+      };
+      this.httpClient.post('http://localhost:3001', body, options)
+        .subscribe(response => {
+          console.log(response);
+        });
     });
   }
 
