c756fe67b608f59aab0f0919e5303271a68e87a5
boberneprotiv <boberneprotiv@gmail.com>
2022-12-30 19:04:25 +0200

    clear DateAgoPipe pipe

--- src/app/modules/shared/components/news-list/pipes/date-ago-pipe/date-ago.pipe.ts
+++ src/app/modules/shared/components/news-list/pipes/date-ago-pipe/date-ago.pipe.ts
@@ -1,46 +1,46 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { DatePipe } from '@angular/common'
+import { DatePipe } from '@angular/common';
 
 @Pipe({
   name: 'dateAgo'
 })
 export class DateAgoPipe implements PipeTransform {
-    transform(value: string, args?: any): string {
-        if (!value) {
-            return value;
-        }
-        
-        const seconds = Math.floor((+new Date() - +new Date(value)) / 1000);
-        
-        if (seconds < 29) { // less than 30 seconds ago will show as 'Just now'
-            return 'Just now';
-        }
-        
-        let intervals = new Map<string, number>([
-            ['year', 31536000],
-            ['month', 2592000],
-            ['week', 604800],
-            ['day', 86400],
-            ['hour', 3600],
-            ['minute', 60],
-            ['second', 1]
-        ]);
-
-        const twoDaysInSecond = intervals.get('day') * 2;
-        
-        for (let item of intervals) {
-            let counter = Math.floor(seconds / item[1]);
-            if (counter > 0) {
-                if (args && args == 'publication' && seconds >= twoDaysInSecond) {
-                    let datePipe = new DatePipe(`en-US`);
-                    return datePipe.transform(value, 'd-MMM-yyyy');
-                }
-
-                value = `${counter} ${item[0]}${ counter === 1 ? ' ago' : 's ago' }`;
-                break;
-            } 
-        }
-
-        return value;
+  transform(value: string, args?: any): string {
+    if (!value) {
+      return value;
     }
+
+    const seconds = Math.floor((+new Date() - +new Date(value)) / 1000);
+
+    if (seconds < 29) { // less than 30 seconds ago will show as 'Just now'
+      return 'Just now';
+    }
+
+    const intervals = new Map<string, number>([
+      ['year', 31536000],
+      ['month', 2592000],
+      ['week', 604800],
+      ['day', 86400],
+      ['hour', 3600],
+      ['minute', 60],
+      ['second', 1]
+    ]);
+
+    const twoDaysInSecond = intervals.get('day') * 2;
+
+    for (const item of intervals) {
+      const counter = Math.floor(seconds / item[1]);
+      if (counter > 0) {
+        if (args && args === 'publication' && seconds >= twoDaysInSecond) {
+          const datePipe = new DatePipe(`en-US`);
+          return datePipe.transform(value, 'd-MMM-yyyy');
+        }
+
+        value = `${counter} ${item[0]}${counter === 1 ? ' ago' : 's ago'}`;
+        break;
+      }
+    }
+
+    return value;
+  }
 }
